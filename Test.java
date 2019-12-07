f094c90b7a0b9a04c8ba10c7cce5bfa3d33fabc0
isqr4 <isqr44@gmail.com>
2019-12-07 14:51:39 +0000

    Revert "Revert "Test.java""
    This reverts commit e99490f6e2c0a92e2ffc5678a99d88cefbd1895e.

---
+++ Test.java
@@ -0,0 +1,15 @@
+public class Test {
+    public static void main(String[] args) {
+        for (String s: args) {
+            int i = 45;
+            System.out.println(++i + " ");
+            System.out.println(s);
+            System.out.println(" aaa");
+            System.out.println(" aaa");
+
+
+        }
+    }
+
+
+}
